CarbonDev/android_packages_apps_CarbonFibers

Add Button Settings, Backlight
Change-Id: I2a13d8d86e4205cf4b9b60a94964deff6bf9fd79

src/com/carbon/fibers/SettingsActivity.java
@@ -36,11 +36,13 @@ import android.widget.ListAdapter;
 import android.widget.Switch;
 import android.widget.TextView;
 
-public class CrSettingsActivity extends PreferenceActivity implements ButtonBarHandler {
+import com.carbon.fibers.fragments.ButtonSettings;
+
+public class SettingsActivity extends PreferenceActivity implements ButtonBarHandler {
 
     private static final String TAG = "CR_Settings";
 
-//    private static String KEY_USE_ENGLISH_LOCALE = "use_english_locale";
+    private static String KEY_USE_ENGLISH_LOCALE = "use_english_locale";
 
     protected HashMap<Integer, Integer> mHeaderIndexMap = new HashMap<Integer, Integer>();
     private List<Header> mHeaders;
@@ -84,7 +86,7 @@ public class CrSettingsActivity extends PreferenceActivity implements ButtonBarH
             }
 
             try {
-                cls.asSubclass(CrSettingsActivity.class);
+                cls.asSubclass(SettingsActivity.class);
                 return;
             } catch (ClassCastException e) {
                 // fall through
@@ -155,7 +157,7 @@ public class CrSettingsActivity extends PreferenceActivity implements ButtonBarH
                 recreate();
                 return true;
             case android.R.id.home:
-                // think of an idea to make the navigation drawer toggle button works.
+                onBackPressed();
                 return true;
             default:
                 return super.onContextItemSelected(item);
@@ -184,6 +186,20 @@ public class CrSettingsActivity extends PreferenceActivity implements ButtonBarH
                     getBaseContext().getResources().getDisplayMetrics());
 
         }
+    }
+
+    private static final String[] ENTRY_FRAGMENTS = {
+        ButtonSettings.class.getName(),
+    };
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        // Almost all fragments are wrapped in this,
+        // except for a few that have their own activities.
+        for (int i = 0; i < ENTRY_FRAGMENTS.length; i++) {
+            if (ENTRY_FRAGMENTS[i].equals(fragmentName)) return true;
+        }
+        return false;
     }
 
     /**
@@ -262,7 +278,7 @@ public class CrSettingsActivity extends PreferenceActivity implements ButtonBarH
     public void onResume() {
         super.onResume();
 
-//        setLocale();
+        setLocale();
 
         ListAdapter listAdapter = getListAdapter();
         if (listAdapter instanceof HeaderAdapter) {

src/com/carbon/fibers/fragments/BacklightTimeoutSeekBar.java
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.fibers.fragments;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+public class BacklightTimeoutSeekBar extends SeekBar {
+    private int mMax;
+    private int mGap;
+    private boolean mUpdatingThumb;
+
+    public BacklightTimeoutSeekBar(Context context) {
+        super(context);
+    }
+
+    public BacklightTimeoutSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BacklightTimeoutSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        mUpdatingThumb = true;
+        super.onSizeChanged(w, h, oldw, oldh);
+        mUpdatingThumb = false;
+    }
+
+    @Override
+    public void setThumb(Drawable thumb) {
+        mUpdatingThumb = true;
+        super.setThumb(thumb);
+        mUpdatingThumb = false;
+    }
+
+    @Override
+    public void setMax(int max) {
+        mMax = max;
+        mGap = max / 10;
+        super.setMax(max + 2 * mGap - 1);
+    }
+
+    @Override
+    public void setProgress(int progress) {
+        if (progress == 0) {
+            progress = getMax();
+        } else {
+            progress--;
+        }
+        super.setProgress(progress);
+    }
+
+    @Override
+    public int getProgress() {
+        int progress = super.getProgress();
+        if (mUpdatingThumb) {
+            return progress;
+        } else if (progress == getMax()) {
+            return 0;
+        }
+        return progress + 1;
+    }
+
+    @Override
+    protected int updateTouchProgress(int lastProgress, int newProgress) {
+        if (newProgress < mMax) {
+            return newProgress;
+        }
+        if (newProgress < mMax + mGap) {
+            return mMax - 1;
+        }
+        return getMax();
+    }
+}

src/com/carbon/fibers/preference/SettingsPreferenceFragment.java
@@ -37,6 +37,8 @@ import android.telephony.TelephonyManager;
 import android.widget.Button;
 
 import com.carbon.fibers.R;
+import com.carbon.fibers.ButtonBarHandler;
+import com.carbon.fibers.DialogCreatable;
 
 /**
  * Base class for Settings fragments, with some helper functions and dialog management
@@ -47,10 +49,6 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     protected Context mContext;
 
     private SettingsDialogFragment mDialogFragment;
-    protected boolean hasTorch;
-    protected boolean hasHardwareButtons;
-    protected boolean hasFastCharge;
-    protected boolean hasColorTuning;
     protected boolean hasVibration = false;
     protected ContentResolver mContentRes;
     protected ContentResolver mContentAppRes;
@@ -58,10 +56,6 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        hasTorch = getResources().getBoolean(R.bool.has_torch);
-        hasHardwareButtons = getResources().getBoolean(R.bool.has_hardware_buttons);
-        hasFastCharge = getResources().getBoolean(R.bool.has_fast_charge);
-        hasColorTuning = getResources().getBoolean(R.bool.has_color_tuning);
 
         mContext = getActivity();
         mContentRes = getActivity().getContentResolver();

src/com/carbon/fibers/preference/SystemSettingCheckBoxPreference.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.fibers.preference;
+
+import android.content.Context;
+import android.preference.CheckBoxPreference;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class SystemSettingCheckBoxPreference extends CheckBoxPreference {
+    public SystemSettingCheckBoxPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SystemSettingCheckBoxPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SystemSettingCheckBoxPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+
+            Settings.System.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+
+        return Settings.System.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}

OctoStash/platform_packages_apps_Tentacles

Conflicts:
	AndroidManifest.xml
	res/mipmap-hdpi/ic_launcher.png
	res/mipmap-mdpi/ic_launcher.png
	res/mipmap-xhdpi/ic_launcher.png
	res/mipmap-xxhdpi/ic_launcher.png
	res/xml/preference_headers.xml
	src/com/oct/tentacles/CreateShortcut.java
	src/com/oct/tentacles/SettingsActivity.java
	src/com/oct/tentacles/preference/ApplicationsDialogPreference.java
	src/com/oct/tentacles/preference/SettingsPreferenceFragment.java

src/com/oct/tentacles/SettingsActivity.java
@@ -36,11 +36,13 @@ import android.widget.ListAdapter;
 import android.widget.Switch;
 import android.widget.TextView;
 
-public class OctSettingsActivity extends PreferenceActivity implements ButtonBarHandler {
+import com.oct.tentacles.fragments.ButtonSettings;
+
+public class SettingsActivity extends PreferenceActivity implements ButtonBarHandler {
 
     private static final String TAG = "Oct_Settings";
 
-//    private static String KEY_USE_ENGLISH_LOCALE = "use_english_locale";
+    private static String KEY_USE_ENGLISH_LOCALE = "use_english_locale";
 
     protected HashMap<Integer, Integer> mHeaderIndexMap = new HashMap<Integer, Integer>();
     private List<Header> mHeaders;
@@ -84,7 +86,7 @@ public class OctSettingsActivity extends PreferenceActivity implements ButtonBar
             }
 
             try {
-                cls.asSubclass(CrSettingsActivity.class);
+                cls.asSubclass(SettingsActivity.class);
                 return;
             } catch (ClassCastException e) {
                 // fall through
@@ -155,7 +157,7 @@ public class OctSettingsActivity extends PreferenceActivity implements ButtonBar
                 recreate();
                 return true;
             case android.R.id.home:
-                // think of an idea to make the navigation drawer toggle button works.
+                onBackPressed();
                 return true;
             default:
                 return super.onContextItemSelected(item);
@@ -184,6 +186,20 @@ public class OctSettingsActivity extends PreferenceActivity implements ButtonBar
                     getBaseContext().getResources().getDisplayMetrics());
 
         }
+    }
+
+    private static final String[] ENTRY_FRAGMENTS = {
+        ButtonSettings.class.getName(),
+    };
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        // Almost all fragments are wrapped in this,
+        // except for a few that have their own activities.
+        for (int i = 0; i < ENTRY_FRAGMENTS.length; i++) {
+            if (ENTRY_FRAGMENTS[i].equals(fragmentName)) return true;
+        }
+        return false;
     }
 
     /**
@@ -262,7 +278,7 @@ public class OctSettingsActivity extends PreferenceActivity implements ButtonBar
     public void onResume() {
         super.onResume();
 
-//        setLocale();
+        setLocale();
 
         ListAdapter listAdapter = getListAdapter();
         if (listAdapter instanceof HeaderAdapter) {

src/com/oct/tentacles/fragments/BacklightTimeoutSeekBar.java
@@ -1,0 +1,93 @@
+/*
+ * Copyright (C) 2014 Team OctOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oct.tentacles.fragments;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.SeekBar;
+
+public class BacklightTimeoutSeekBar extends SeekBar {
+    private int mMax;
+    private int mGap;
+    private boolean mUpdatingThumb;
+
+    public BacklightTimeoutSeekBar(Context context) {
+        super(context);
+    }
+
+    public BacklightTimeoutSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BacklightTimeoutSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        mUpdatingThumb = true;
+        super.onSizeChanged(w, h, oldw, oldh);
+        mUpdatingThumb = false;
+    }
+
+    @Override
+    public void setThumb(Drawable thumb) {
+        mUpdatingThumb = true;
+        super.setThumb(thumb);
+        mUpdatingThumb = false;
+    }
+
+    @Override
+    public void setMax(int max) {
+        mMax = max;
+        mGap = max / 10;
+        super.setMax(max + 2 * mGap - 1);
+    }
+
+    @Override
+    public void setProgress(int progress) {
+        if (progress == 0) {
+            progress = getMax();
+        } else {
+            progress--;
+        }
+        super.setProgress(progress);
+    }
+
+    @Override
+    public int getProgress() {
+        int progress = super.getProgress();
+        if (mUpdatingThumb) {
+            return progress;
+        } else if (progress == getMax()) {
+            return 0;
+        }
+        return progress + 1;
+    }
+
+    @Override
+    protected int updateTouchProgress(int lastProgress, int newProgress) {
+        if (newProgress < mMax) {
+            return newProgress;
+        }
+        if (newProgress < mMax + mGap) {
+            return mMax - 1;
+        }
+        return getMax();
+    }
+}

src/com/oct/tentacles/preference/SettingsPreferenceFragment.java
@@ -37,6 +37,8 @@ import android.telephony.TelephonyManager;
 import android.widget.Button;
 
 import com.oct.tentacles.R;
+import com.oct.tentacles.ButtonBarHandler;
+import com.oct.tentacles.DialogCreatable;
 
 /**
  * Base class for Settings fragments, with some helper functions and dialog management
@@ -47,10 +49,6 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     protected Context mContext;
 
     private SettingsDialogFragment mDialogFragment;
-    protected boolean hasTorch;
-    protected boolean hasHardwareButtons;
-    protected boolean hasFastCharge;
-    protected boolean hasColorTuning;
     protected boolean hasVibration = false;
     protected ContentResolver mContentRes;
     protected ContentResolver mContentAppRes;
@@ -58,10 +56,6 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        hasTorch = getResources().getBoolean(R.bool.has_torch);
-        hasHardwareButtons = getResources().getBoolean(R.bool.has_hardware_buttons);
-        hasFastCharge = getResources().getBoolean(R.bool.has_fast_charge);
-        hasColorTuning = getResources().getBoolean(R.bool.has_color_tuning);
 
         mContext = getActivity();
         mContentRes = getActivity().getContentResolver();

src/com/oct/tentacles/preference/SystemSettingCheckBoxPreference.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2014 Team OctOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oct.tentacles.preference;
+
+import android.content.Context;
+import android.preference.CheckBoxPreference;
+import android.provider.Settings;
+import android.util.AttributeSet;
+
+public class SystemSettingCheckBoxPreference extends CheckBoxPreference {
+    public SystemSettingCheckBoxPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SystemSettingCheckBoxPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SystemSettingCheckBoxPreference(Context context) {
+        super(context, null);
+    }
+
+    @Override
+    protected boolean persistBoolean(boolean value) {
+        if (shouldPersist()) {
+            if (value == getPersistedBoolean(!value)) {
+                // It's already there, so the same as persisting
+                return true;
+            }
+
+            Settings.System.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
+        if (!shouldPersist()) {
+            return defaultReturnValue;
+        }
+
+        return Settings.System.getInt(getContext().getContentResolver(),
+                getKey(), defaultReturnValue ? 1 : 0) != 0;
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        // Using getString instead of getInt so we can simply check for null
+        // instead of catching an exception. (All values are stored as strings.)
+        return Settings.System.getString(getContext().getContentResolver(), getKey()) != null;
+    }
+}