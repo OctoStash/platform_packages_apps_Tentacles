CarbonDev/android_packages_apps_CarbonFibers

CF: Clear all recents position
- can be set to any of the 4 corners or disabled

Change-Id: I6eac540a8ee3e1a7de636972f8c88b9bb8036369

src/com/carbon/fibers/SettingsActivity.java
@@ -39,6 +39,7 @@ import android.widget.TextView;
 // fragments import for entry
 import com.carbon.fibers.fragments.*;
 import com.carbon.fibers.fragments.sb.*;
+import com.carbon.fibers.fragments.ui.*;
 
 public class SettingsActivity extends PreferenceActivity implements ButtonBarHandler {
 
@@ -193,10 +194,11 @@ public class SettingsActivity extends PreferenceActivity implements ButtonBarHan
 
     private static final String[] ENTRY_FRAGMENTS = {
         ButtonSettings.class.getName(),
+        InterfaceSettings.class.getName(),
         MoreDeviceSettings.class.getName(),
         PowerMenuSettings.class.getName(),
         SbSettings.class.getName(),
-        QuickSettingsTiles.class.getName(),
+        QuickSettingsTiles.class.getName()
     };
 
     @Override

src/com/carbon/fibers/fragments/ui/InterfaceSettings.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2013 Carbon Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.fibers.fragments.ui;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.carbon.fibers.R;
+import com.carbon.fibers.preference.SettingsPreferenceFragment;
+import com.carbon.fibers.Utils;
+
+import com.carbon.fibers.fragments.ui.*;
+
+import java.lang.Exception;
+import java.util.ArrayList;
+
+public class InterfaceSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Interface_Category";
+
+    PagerTabStrip mPagerTabStrip;
+    ViewPager mViewPager;
+
+    String titleString[];
+
+    ViewGroup mContainer;
+
+    static Bundle mSavedState;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.tab_ui_sb, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pagerTabStrip);
+
+        StatusBarAdapter StatusBarAdapter = new StatusBarAdapter(getFragmentManager());
+        mViewPager.setAdapter(StatusBarAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!Utils.isTablet(getActivity())) {
+            mContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    class StatusBarAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public StatusBarAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new RecentsPanel();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                    getString(R.string.ui_recents_category)};
+        return titleString;
+    }
+}

src/com/carbon/fibers/fragments/ui/RecentsPanel.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2012 The Carbon-Rom project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.fibers.fragments.ui;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.carbon.fibers.R;
+import com.carbon.fibers.preference.SettingsPreferenceFragment;
+
+public class RecentsPanel extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "RecentsPanel";
+
+    private static final String CLEAR_RECENTS_BUTTON = "clear_recents_button";
+
+    private ListPreference mClearAllButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.ui_recents_panel);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // clear recents position
+        mClearAllButton = (ListPreference) findPreference(CLEAR_RECENTS_BUTTON);
+        int clearStatus = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.CLEAR_RECENTS_BUTTON, 4);
+        mClearAllButton.setValue(String.valueOf(clearStatus));
+        mClearAllButton.setSummary(mClearAllButton.getEntry());
+        mClearAllButton.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mClearAllButton) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mClearAllButton.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.CLEAR_RECENTS_BUTTON, value);
+            mClearAllButton.setSummary(mClearAllButton.getEntries()[index]);
+            return true;
+        }
+
+        return false;
+    }
+}

OctoStash/platform_packages_apps_Tentacles

Conflicts:
	res/xml/preference_headers.xml
	src/com/oct/tentacles/SettingsActivity.java

src/com/oct/tentacles/SettingsActivity.java
@@ -39,6 +39,7 @@ import android.widget.TextView;
 // fragments import for entry
 import com.oct.tentacles.fragments.*;
 import com.oct.tentacles.fragments.sb.*;
+import com.oct.tentacles.fragments.ui.*;
 
 public class SettingsActivity extends PreferenceActivity implements ButtonBarHandler {
 
@@ -193,9 +194,10 @@ public class SettingsActivity extends PreferenceActivity implements ButtonBarHan
 
     private static final String[] ENTRY_FRAGMENTS = {
         ButtonSettings.class.getName(),
+        InterfaceSettings.class.getName(),
         PowerMenuSettings.class.getName(),
         SbSettings.class.getName(),
-        QuickSettingsTiles.class.getName(),
+        QuickSettingsTiles.class.getName()
     };
 
     @Override

src/com/oct/tentacles/fragments/ui/InterfaceSettings.java
@@ -1,0 +1,117 @@
+/*
+ * Copyright (C) 2013 Carbon Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oct.tentacles.fragments.ui;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.oct.tentacles.R;
+import com.oct.tentacles.preference.SettingsPreferenceFragment;
+import com.oct.tentacles.Utils;
+
+import com.oct.tentacles.fragments.ui.*;
+
+import java.lang.Exception;
+import java.util.ArrayList;
+
+public class InterfaceSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "Interface_Category";
+
+    PagerTabStrip mPagerTabStrip;
+    ViewPager mViewPager;
+
+    String titleString[];
+
+    ViewGroup mContainer;
+
+    static Bundle mSavedState;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        mContainer = container;
+
+        View view = inflater.inflate(R.layout.tab_ui_sb, container, false);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        mPagerTabStrip = (PagerTabStrip) view.findViewById(R.id.pagerTabStrip);
+
+        StatusBarAdapter StatusBarAdapter = new StatusBarAdapter(getFragmentManager());
+        mViewPager.setAdapter(StatusBarAdapter);
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        // After confirming PreferenceScreen is available, we call super.
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (!Utils.isTablet(getActivity())) {
+            mContainer.setPadding(0, 0, 0, 0);
+        }
+    }
+
+    class StatusBarAdapter extends FragmentPagerAdapter {
+        String titles[] = getTitles();
+        private Fragment frags[] = new Fragment[titles.length];
+
+        public StatusBarAdapter(FragmentManager fm) {
+            super(fm);
+            frags[0] = new RecentsPanel();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return frags.length;
+        }
+    }
+
+    private String[] getTitles() {
+        String titleString[];
+        titleString = new String[]{
+                    getString(R.string.ui_recents_category)};
+        return titleString;
+    }
+}

src/com/oct/tentacles/fragments/ui/RecentsPanel.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2012 The Carbon-Rom project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oct.tentacles.fragments.ui;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+
+import com.oct.tentacles.R;
+import com.oct.tentacles.preference.SettingsPreferenceFragment;
+
+public class RecentsPanel extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+    private static final String TAG = "RecentsPanel";
+
+    private static final String CLEAR_RECENTS_BUTTON = "clear_recents_button";
+
+    private ListPreference mClearAllButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.ui_recents_panel);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // clear recents position
+        mClearAllButton = (ListPreference) findPreference(CLEAR_RECENTS_BUTTON);
+        int clearStatus = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.CLEAR_RECENTS_BUTTON, 4);
+        mClearAllButton.setValue(String.valueOf(clearStatus));
+        mClearAllButton.setSummary(mClearAllButton.getEntry());
+        mClearAllButton.setOnPreferenceChangeListener(this);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mClearAllButton) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mClearAllButton.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.CLEAR_RECENTS_BUTTON, value);
+            mClearAllButton.setSummary(mClearAllButton.getEntries()[index]);
+            return true;
+        }
+
+        return false;
+    }
+}