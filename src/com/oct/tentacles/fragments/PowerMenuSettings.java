CarbonDev/android_packages_apps_CarbonFibers

Power Menu Settings (CM)
Change-Id: I0a447d8d8503e51a763df50b158a055895925495

src/com/carbon/fibers/fragments/PowerMenuSettings.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2012 CyanogenMod
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.carbon.fibers.fragments;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.provider.Settings;
+
+import com.carbon.fibers.R;
+import com.carbon.fibers.preference.SettingsPreferenceFragment;
+
+public class PowerMenuSettings extends SettingsPreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_settings);
+
+        final ContentResolver resolver = getContentResolver();
+
+        // Only enable profiles item if System Profiles are also enabled
+        findPreference(Settings.System.POWER_MENU_PROFILES_ENABLED).setEnabled(
+                Settings.System.getInt(resolver, Settings.System.SYSTEM_PROFILES_ENABLED, 1) != 0);
+
+        if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+            getPreferenceScreen().removePreference(
+                    findPreference(Settings.System.POWER_MENU_USER_ENABLED));
+        }
+    }
+}

OctoStash/platform_packages_apps_Tentacles

Conflicts:
	src/com/oct/tentacles/SettingsActivity.java

src/com/oct/tentacles/fragments/PowerMenuSettings.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2014 CarbonDev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.oct.tentacles.fragments;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.provider.Settings;
+
+import com.oct.tentacles.R;
+import com.oct.tentacles.preference.SettingsPreferenceFragment;
+
+public class PowerMenuSettings extends SettingsPreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.power_menu_settings);
+
+        final ContentResolver resolver = getContentResolver();
+
+        // Only enable profiles item if System Profiles are also enabled
+        findPreference(Settings.System.POWER_MENU_PROFILES_ENABLED).setEnabled(
+                Settings.System.getInt(resolver, Settings.System.SYSTEM_PROFILES_ENABLED, 1) != 0);
+
+        if (!UserHandle.MU_ENABLED || !UserManager.supportsMultipleUsers()) {
+            getPreferenceScreen().removePreference(
+                    findPreference(Settings.System.POWER_MENU_USER_ENABLED));
+        }
+    }
+}