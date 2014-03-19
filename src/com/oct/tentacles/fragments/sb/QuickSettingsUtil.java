Carbon-m7/CarbonFibers

Immersive Mode tile (2/2)
Change-Id: I6491a9b16aa0726df2a8ea7a82282e667ecbf57c

src/com/carbon/fibers/fragments/sb/QuickSettingsUtil.java
@@ -26,6 +26,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_CAMERA;
 import static com.android.internal.util.cm.QSConstants.TILE_DELIMITER;
 import static com.android.internal.util.cm.QSConstants.TILE_EXPANDEDDESKTOP;
 import static com.android.internal.util.cm.QSConstants.TILE_GPS;
+import static com.android.internal.util.cm.QSConstants.TILE_IMMERSIVE;
 import static com.android.internal.util.cm.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.cm.QSConstants.TILE_LTE;
 import static com.android.internal.util.cm.QSConstants.TILE_MOBILEDATA;
@@ -157,6 +158,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_NETWORKADB, R.string.title_tile_network_adb,
                 "com.android.systemui:drawable/ic_qs_network_adb_off"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_IMMERSIVE, R.string.title_tile_immersive,
+                "com.android.systemui:drawable/ic_qs_immersive_off"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {

OctoStash/platform_packages_apps_Tentacles

Conflicts:
	res/values/strings.xml
	res/xml/power_menu_settings.xml
	src/com/oct/tentacles/fragments/sb/QuickSettingsUtil.java

src/com/oct/tentacles/fragments/sb/QuickSettingsUtil.java
@@ -27,6 +27,7 @@ import static com.android.internal.util.oct.QSConstants.TILE_BRIGHTNESS;
 import static com.android.internal.util.oct.QSConstants.TILE_CAMERA;
 import static com.android.internal.util.oct.QSConstants.TILE_DELIMITER;
 import static com.android.internal.util.oct.QSConstants.TILE_GPS;
+import static com.android.internal.util.oct.QSConstants.TILE_IMMERSIVE;
 import static com.android.internal.util.oct.QSConstants.TILE_LOCKSCREEN;
 import static com.android.internal.util.oct.QSConstants.TILE_LTE;
 import static com.android.internal.util.oct.QSConstants.TILE_MOBILEDATA;
@@ -43,6 +44,7 @@ import static com.android.internal.util.oct.QSConstants.TILE_USER;
 import static com.android.internal.util.oct.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.oct.QSConstants.TILE_WIFI;
 import static com.android.internal.util.oct.QSConstants.TILE_WIFIAP;
+
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -139,6 +141,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_WIFIAP, R.string.title_tile_wifiap,
                 "com.android.systemui:drawable/ic_qs_wifi_ap_on"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_IMMERSIVE, R.string.title_tile_immersive,
+                "com.android.systemui:drawable/ic_qs_immersive_off"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {